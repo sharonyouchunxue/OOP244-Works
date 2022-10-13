1e807a90536c180ab6d958cf02099998b74a5062
sharonyouchunxue <cyou8@myseneca.ca>
2022-10-13 16:48:14 -0400

    DIY CREATED..

---
+++ Workshops/w5/diy/Mark.h
@@ -0,0 +1,30 @@
+#pragma once
+/*
+*****************************************************************************
+                        Workshop 5 - part 1
+Full Name  : Chunxue You
+Student ID#: 127632214
+Email      : cyou8@myseneca.ca
+Section    : OOP244 NAA
+Date       : 2022-10-12
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. I have done all
+the coding by myself and only copied the code that my professor provided to
+complete my workshops and assignments.
+*****************************************************************************
+*/
+#pragma once
+#ifndef SDDS_MARK_H_
+#define SDDS_MARK_H_
+#include <iostream>
+
+namespace sdds
+{
+
+
+
+
+}
+#endif
