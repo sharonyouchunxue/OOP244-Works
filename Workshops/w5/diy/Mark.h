d45abd420716decb968c8ced37e566fb2c0feedc
sharonyouchunxue <cyou8@myseneca.ca>
2022-10-14 21:58:56 -0400

    update

--- Workshops/w5/diy/Mark.h
+++ Workshops/w5/diy/Mark.h
@@ -6,7 +6,7 @@ Full Name  : Chunxue You
 Student ID#: 127632214
 Email      : cyou8@myseneca.ca
 Section    : OOP244 NAA
-Date       : 2022-10-12
+Date       : 2022-10-14
 
 Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
@@ -20,9 +20,42 @@ complete my workshops and assignments.
 #define SDDS_MARK_H_
 #include <iostream>
 
+
 namespace sdds
 {
+    class Mark {
+    private:
+        int m_mark{};
+    public:
+        Mark();
+        ~Mark();
+        Mark(const int value);
+        operator int()const;
+        operator double()const;
+        operator char()const;
+        operator bool()const;
+        //== , != , <, >, <= and >=
+        bool operator==(const Mark& rightOperand);
+        bool operator!=(const Mark& rightOperand);
+        bool operator<(const Mark& rightOperand);
+        bool operator>(const Mark& rightOperand);
+        bool operator<=(const Mark& rightOperand);
+        bool operator>=(const Mark& rightOperand);
+        Mark& operator++();
+        Mark operator++(int);
+        Mark& operator--();
+        Mark operator--(int);
+        bool operator~()const;
+        Mark& operator<<(Mark& rightOperand);
+        Mark& operator>>(Mark& leftOperand);
+        Mark& operator=(int value);
+        Mark& operator+=(int value);
+        Mark& operator-=(int value);
+        friend int operator +=(int& lefOperand, const Mark& rightOperand);
+        
+        
 
+    };
 
 
 
